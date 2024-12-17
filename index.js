5af017543eb5b6f2c3d154dace2fb47bc9fa759c
TheQuestioningCake <perrywayne39@gmail.com>
2024-12-17 08:10:44 -0500

    added an ascii

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import chalk from 'chalk';
 import playerState from './utils/player-state.js';
 import { adventure, directionPrompt } from './utils/adventure.js';
 import { handleDirectionChoice } from './utils/handle-direction.js';
+import ascii from './utils/ascii.js'
 
 inquirer
     .prompt(adventure.slice(0, 1))
     .then(answers => {
         playerState.name = answers['player-name'];
-        console.log(`Welcome, ${playerState.name}.`);
+        console.log(chalk.red.bold(ascii))
+        console.log(`Welcome, to your death ${playerState.name}.`);
         return inquirer.prompt(adventure.slice(1, 2));
     })
     .then(initialWeaponChoice => {
