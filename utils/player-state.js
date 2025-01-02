db8dec85a964bd0df92380e7cd9f2063620ccea8
TheQuestioningCake <perrywayne39@gmail.com>
2025-01-02 13:45:09 -0500

    started working on creating alternate routes, and how to keep track of each route the player has taken

--- utils/player-state.js
+++ utils/player-state.js
@@ -3,6 +3,14 @@ const playerState = {
     weapon: '',
     health: 100,
     inventory: [],
+    vistedLocations: {
+        house1: {
+            hasEntered: false,
+            hasVisitedUpstairs: false,
+            hasVistedKitchen: false,
+            hasVistedLivingroom: false
+        }
+    },
 };
 
 export default playerState;
