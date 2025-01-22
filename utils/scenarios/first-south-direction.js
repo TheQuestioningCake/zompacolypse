6f732e3382a8ea27706b6e1008665b01b373706d
TheQuestioningCake <perrywayne39@gmail.com>
2025-01-22 14:24:44 -0500

    working on the first South encounter, might have to change the story so the first north house is his families house and that he ran away when they were bitten by the zombie upstairs

--- utils/scenarios/first-south-direction.js
+++ utils/scenarios/first-south-direction.js
@@ -4,7 +4,7 @@ import { directionPrompt } from '../adventure.js';
 import playerState from '../player-state.js';
 import { checkVisited, appliedDamage, appliedHealing } from '../helper.js';
 import { medkit } from '../inventory.js';
-import { firstSouth } from './first-south-scenarios.js';
+import { firstSouth, firstSouthEncounter } from './first-south-scenarios.js';
 import { chooseDirection } from '../handle-direction.js';
 
 export function handleFirstSouth() {
@@ -14,9 +14,22 @@ export function handleFirstSouth() {
         .then(firstSouthAnswers => {
             if (firstSouthAnswers.firstSouth === 'Yes') {
                 console.log(`You hesitantly approach the figure. Palms sweaty from the death grip you have one your $ `)
+                return handleFirstSouthEncounter()
             } else {
                 console.log('You decide to turn back.');
                 return chooseDirection();
+            }
+        })
+}
+
+export function handleFirstSouthEncounter() {
+    return inquirer
+        .prompt(firstSouthEncounter)
+        .then(firstEncounterAnswers => {
+            if(firstEncounterAnswers.firstSouthEncounter === 'Yes') {
+                console.log(`You tell the man your name is ${playerState.name}`)
+            } else {
+                console.log(`He looks at you bewildered, you can tell he was just looking for some human decency in this madness`)
             }
         })
 }
--- utils/scenarios/first-south-scenarios.js
+++ utils/scenarios/first-south-scenarios.js
@@ -8,3 +8,12 @@ export const firstSouth = [
         choices: ['Yes', 'No']
     }
 ]
+
+export const firstSouthEncounter = [
+    {
+        type: 'list',
+        name: 'firstSouthEncounter',
+        message:`In front of you is a disheveled man. He's breaking down, crying... Then he looks up to you. He ask who you are. Do you tell him your name?`,
+        choices: ['Yes', 'No']
+    }
+]
